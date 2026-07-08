477eaeb753372fb40de7c4526543c65d18ceceed
KESHAV GUPTA <keshavgupta3354@gmail.com>
2026-07-08 23:32:25 +0530

    Update script.js

--- 25ESKCX048/DAY5/script.js
+++ 25ESKCX048/DAY5/script.js
@@ -1,1 +1,10 @@
 
+let count = 0;
+
+let button = document.getElementById("clickBtn");
+let counter = document.getElementById("count");
+
+button.addEventListener("click", function () {
+    count++;
+    counter.innerHTML = count;
+});
